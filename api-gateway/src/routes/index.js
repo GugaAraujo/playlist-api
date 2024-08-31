8c7d06b9af17beafe4d5cf19ff11f42eeb7a1f73
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2024-08-31 13:34:17 -0300

    feat(api-gateway): add logged user to options headers

--- api-gateway/src/routes/index.js
+++ api-gateway/src/routes/index.js
@@ -4,10 +4,28 @@ const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
-router.use("/playlists", authMiddleware, proxy("http://playlist-service:3000"));
-router.use("/analysis", authMiddleware, proxy("http://analysis-service:3001"));
+const addUserIdHeader = (opts, req) => {
+  if (req.user) {
+    opts.headers["x-user"] = JSON.stringify(req.user);
+  }
+  return opts;
+};
 
-router.use("/auth", proxy("http://auth-service:3002"));
+const createProxy = (serviceUrl) =>
+  proxy(serviceUrl, {
+    proxyReqOptDecorator: addUserIdHeader,
+  });
 
+router.use(
+  "/playlists",
+  authMiddleware,
+  createProxy("http://playlist-service:3000")
+);
+router.use(
+  "/analysis",
+  authMiddleware,
+  createProxy("http://analysis-service:3001")
+);
+router.use("/auth", createProxy("http://auth-service:3002"));
 
 module.exports = router;
