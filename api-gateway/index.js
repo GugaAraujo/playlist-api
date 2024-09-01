0c6f6028ab3ff23e5bb4e47e2a52332b3042b80b
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2024-09-01 03:11:43 -0300

    feat(api-gateway): set cors and rate limite

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const routes = require("./src/routes");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 dotenv.config();
 
+const limiter = rateLimit({
+  windowMs: 3 * 60 * 1000,
+  max: 60,
+  message: "Too many requests from this IP, please try again after 3 minutes",
+});
+
+const corsOptions = {};
+if (process.env.NODE_ENV === "production") {
+  corsOptions["origin"] = process.env.ALLOWED_DOMAIN;
+} else {
+  corsOptions["origin"] = "*";
+}
+
 const app = express();
+app.use(cors(corsOptions));
+app.use(limiter);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
