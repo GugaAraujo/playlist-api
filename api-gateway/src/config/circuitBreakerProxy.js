4d3c0eb3cfe213864114e08add52690f7d114150
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2024-09-01 21:54:06 -0300

    feat(api-gateway): implements proxy with Circuit Breaker and send logs to Winston

---
+++ api-gateway/src/config/circuitBreakerProxy.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+const CircuitBreaker = require("opossum");
+const logger = require("./logger");
+
+const addUserIdHeader = (headers, req) => {
+  if (req.user) {
+    headers["x-user"] = JSON.stringify(req.user);
+  }
+  return headers;
+};
+
+
+const createCircuitBreaker = (serviceUrl) => {
+  const options = {
+    timeout: 3000,
+    errorThresholdPercentage: 50,
+    resetTimeout: 30000,
+  };
+
+  const axiosInstance = axios.create({
+    baseURL: serviceUrl,
+    timeout: options.timeout,
+  });
+
+  const circuitBreaker = new CircuitBreaker((req) => {
+    const headers = addUserIdHeader({}, req);
+    return axiosInstance({
+      method: req.method,
+      url: req.url,
+      headers: headers,
+      data: req.body,
+    }).then((response) => response.data);
+  }, options);
+
+  Object.entries({
+    open: "warn",
+    halfOpen: "info",
+    close: "info",
+    fallback: "warn",
+    success: "info",
+    failure: "error",
+  }).forEach(([event, level]) => {
+    circuitBreaker.on(event, () => {
+      logger[level](`Circuit breaker ${event} for ${serviceUrl}`);
+    });
+  });
+
+  const handleRequest = (req, res) => {
+    circuitBreaker
+      .fire(req)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((error) => {
+        logger.error(`Request failed: ${error.message}`);
+        res.status(502).send("Bad Gateway");
+      });
+  };
+
+  return handleRequest;
+};
+
+module.exports = createCircuitBreaker;
--- api-gateway/src/routes/index.js
+++ api-gateway/src/routes/index.js
@@ -1,31 +1,19 @@
 const express = require("express");
-const proxy = require("express-http-proxy");
 const authMiddleware = require("../middleware/auth");
+const createBreakerProxy = require("./../config/circuitBreakerProxy");
 
 const router = express.Router();
 
-const addUserIdHeader = (opts, req) => {
-  if (req.user) {
-    opts.headers["x-user"] = JSON.stringify(req.user);
-  }
-  return opts;
-};
-
-const createProxy = (serviceUrl) =>
-  proxy(serviceUrl, {
-    proxyReqOptDecorator: addUserIdHeader,
-  });
-
-router.use(
-  "/playlists",
-  authMiddleware,
-  createProxy("http://playlist-service:3003")
+router.use("/playlists", authMiddleware, (req, res, next) =>
+  createBreakerProxy("http://playlist-service:3003")(req, res, next)
 );
-router.use(
-  "/analysis",
-  authMiddleware,
-  createProxy("http://analysis-service:3001")
+
+router.use("/analysis", authMiddleware, (req, res, next) =>
+  createBreakerProxy("http://analysis-service:3001")(req, res, next)
 );
-router.use("/auth", createProxy("http://auth-service:3002"));
+
+router.use("/auth", (req, res, next) =>
+  createBreakerProxy("http://auth-service:3002")(req, res, next)
+);
 
 module.exports = router;
