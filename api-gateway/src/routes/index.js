a0e6ee2cd416ce7ca4bef3f546b56bbca7d7e2c3
Gustavo Araujo <gustavoaraujo.dev@gmail.com>
2024-09-02 04:08:50 -0300

    doc: implements swagger to document all endpoints

--- api-gateway/src/routes/index.js
+++ api-gateway/src/routes/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth");
 const createBreakerProxy = require("./../config/circuitBreakerProxy");
 
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const path = require("path");
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "swagger.json"), "utf8")
+);
+
 const router = express.Router();
 
 router.use("/playlists", authMiddleware, (req, res, next) =>
@@ -15,5 +23,7 @@ router.use("/analysis", authMiddleware, (req, res, next) =>
 router.use("/auth", (req, res, next) =>
   createBreakerProxy("http://auth-service:3002")(req, res, next)
 );
+
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
